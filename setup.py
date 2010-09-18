fa235bf50fc5b3af1ff06ac5ed56567f0c4f5417
nferrier <none@none>
2010-09-18 11:25:33 +0100

    python build system depends

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Utilities',
+]
+
+setup(
+    name = "veh",
+    version = "0.1",
+    description = "virtualenv for hg",
+    long_description = """Tie virtualenvs to individual mercurial repositorys.""",
+    license = "GNU GPL v3",
+    author = "Nic Ferrier",
+    author_email = "nic@ferrier.me.uk",
+    url = "http://github.com/nicferrier/veh",
+    download_url="http://github.com/nicferrier/veh/downloads",
+    platforms = ["any"],
+    scripts=['src/veh'],
+    classifiers =  classifiers
+    )
