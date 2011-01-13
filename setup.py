68c1078fd326508b9d06f71896c4e9c8eb3b9f9e
nferrier <none@none>
2011-01-13 16:22:41 +0000

    release 0.4 includes ed's changes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name = "veh",
-    version = "0.3",
+    version = "0.4",
     description = "virtualenv for hg",
     long_description = """Tie virtualenvs to individual mercurial repositorys.""",
     license = "GNU GPL v3",
