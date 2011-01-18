da4bcbccc06649703b3c8007fa5d1561cde74ceb
nferrier <none@none>
2011-01-18 13:35:50 +0000

    new release with control for pip

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name = "veh",
-    version = "0.4",
+    version = "0.5",
     description = "virtualenv for hg",
     long_description = """Tie virtualenvs to individual mercurial repositorys.""",
     license = "GNU GPL v3",
