012b3636c67518a373439c5225629a5cd6c898aa
nferrier <none@none>
2011-03-01 23:10:24 +0000

    fix bugs with do_shell and do_active

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name = "veh",
-    version = "0.80",
+    version = "0.81",
     description = "virtualenv for hg",
     long_description = """Tie virtualenvs to individual mercurial repositorys.""",
     license = "GNU GPL v3",
