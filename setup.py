aa3ffbaec47ea76c8299929287ace2cd6239b0b8
nferrier <none@none>
2011-03-02 01:24:54 +0000

    remove pdb from shell command :sadface:

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name = "veh",
-    version = "0.81",
+    version = "0.82",
     description = "virtualenv for hg",
     long_description = """Tie virtualenvs to individual mercurial repositorys.""",
     license = "GNU GPL v3",
